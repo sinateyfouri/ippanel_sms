5751a9f1ccf1b29a7e32d6fd61b06b6a2bad91b8
Sina Teyfouri <sina.teyfouri@gmail.com>
2025-04-01 16:24:32 +0000

    added tag line2

--- models/bulk_sms_wizard.py
+++ models/bulk_sms_wizard.py
@@ -6,17 +6,23 @@ class BulkSmsWizard(models.TransientModel):
     _name = 'bulk.sms.wizard'
     _description = 'Send Bulk SMS to Partners'
 
+    partner_ids = fields.Many2many('res.partner', string="مخاطبین خاص (اختیاری)")
     tag_ids = fields.Many2many('res.partner.category', string="فیلتر تگ مخاطبین")
     message = fields.Text(string="متن پیام", required=True)
 
     def action_send_bulk_sms(self):
-        partners = self.env['res.partner'].search([
-            ('category_id', 'in', self.tag_ids.ids),
-            ('mobile', '!=', False)
-        ])
+        # اگر مخاطبین خاص انتخاب شده بودن، فقط به اون‌ها ارسال کن
+        if self.partner_ids:
+            partners = self.partner_ids.filtered(lambda p: p.mobile)
+        else:
+            # در غیر این صورت، براساس تگ ارسال کن
+            partners = self.env['res.partner'].search([
+                ('category_id', 'in', self.tag_ids.ids),
+                ('mobile', '!=', False)
+            ])
 
         if not partners:
-            raise UserError("هیچ مخاطبی با این تگ‌ها و شماره موبایل پیدا نشد!")
+            raise UserError("هیچ مخاطبی برای ارسال یافت نشد.")
 
         config = self.env['ir.config_parameter'].sudo()
         api_key = config.get_param('ippanel_sms.ippanel_api_key')
@@ -31,7 +37,6 @@ class BulkSmsWizard(models.TransientModel):
                     self.message,
                     "ارسال انبوه پیامک"
                 )
-                # لاگ موفق
                 self.env['sms.log'].create({
                     'partner_id': partner.id,
                     'mobile': partner.mobile,
@@ -39,14 +44,12 @@ class BulkSmsWizard(models.TransientModel):
                     'sender': sender,
                     'status': 'sent',
                 })
-                # در چتر مخاطب ثبت کن
                 partner.message_post(
                     body=f"📨 پیامک انبوه ارسال شد:\n{self.message}",
                     subject="ارسال پیامک با IPPanel",
                     message_type="comment"
                 )
             except Exception as e:
-                # لاگ شکست
                 self.env['sms.log'].create({
                     'partner_id': partner.id,
                     'mobile': partner.mobile,
@@ -55,3 +58,4 @@ class BulkSmsWizard(models.TransientModel):
                     'status': 'failed',
                     'error_message': str(e),
                 })
+
