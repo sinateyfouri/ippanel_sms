83c3f1d9d595f65db0bb0292ad9d36792d16e7d5
Sina Teyfouri <sina.teyfouri@gmail.com>
2025-04-01 11:56:34 +0000

    new files

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Ippanel SMS Integration',
+    'version': '18.0',
+    'category': 'Communication',
+    'summary': 'Send sms with ippanel.com',
+    'author': 'Sina Teyfouri',
+    'website' : 'https://asiapardaz.ir',
+    'depends': ['base', 'contacts', 'account'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/res_config_settings_views.xml'
+
+    ],
+    'application': True,
+    'installable': True,
+}
---
+++ models/res_config_settings.py
@@ -0,0 +1,35 @@
+from odoo import models, fields, api
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+    _description = 'ippanel sms settings'
+
+
+    use_ippanel_sms = fields.Boolean(string="Enable Sending SMS with ippanel", config_parameter='ippanel_sms.use_feature')
+    ippanel_api_key = fields.Char(string="ippannel API")
+    ippanel_sender_number = fields.Char(string="ippanel sender number")
+
+    @api.model
+    def set_values(self):
+        """ذخیره مقادیر در پایگاه داده"""
+        super(ResConfigSettings, self).set_values()
+        params = self.env['ir.config_parameter'].sudo()
+
+        params.set_param('ippannel_sms.use_ippanel_sms', "True" if self.use_ippanel_sms else "False")
+        params.set_param('ippanel_sms.ippanel_api_key', self.api_key or "")
+        params.set_param('ippanel_sms.ippanel_sender_number', self.api_key or "")
+
+
+    @api.model
+    def get_values(self):
+        """خواندن مقادیر از پایگاه داده هنگام باز کردن صفحه تنظیمات"""
+        res = super(ResConfigSettings, self).get_values()
+        params = self.env['ir.config_parameter'].sudo()
+
+        res.update({
+            'use_ippanel_sms': params.get_param('ippannel_sms.use_ippanel_sms', 'False') == "True",
+            'ippanel_api_key': params.get_param('ippanel_sms.ippanel_api_key', default=''),
+            'ippanel_sender_number' : params.get_param('ippanel_sms.ippanel_sender_number', default=''),
+
+        })
+        return res
