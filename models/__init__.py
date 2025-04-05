09002772f9ce771d8cba775dd6cf2227f3a3d891
Sina Teyfouri <sina.teyfouri@gmail.com>
2025-04-05 06:04:00 +0000

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from . import res_config_settings
 from . import res_partner
+from . import sms_template
 from . import send_sms_wizard
 from . import sms_log
 from . import bulk_sms_wizard
-from . import sms_template
